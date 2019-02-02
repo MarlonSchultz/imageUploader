230faf163931a48be4698afd9a93fbe598084339
marlon <spam@marlonschultz.de>
2019-02-02 10:06:07 +0100

    wip; started test for fileuploader

---
+++ Code/PHP/tests/unit/FileUploaderTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mgbs
+ * Date: 01.02.19
+ * Time: 23:16
+ */
+
+namespace App\Test\Unit;
+
+use App\Service\FileUploader;
+
+class FileUploaderTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function test__construct()
+    {
+
+    }
+}
